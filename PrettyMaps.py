74675be0d7160de05e147cc871e1213ddff47553
jaimegonzalez2 <95113326+jaimegonzalez2@users.noreply.github.com>
2022-04-04 17:28:31 -0500

    Create PrettyMaps.py

---
+++ PrettyMaps.py
@@ -0,0 +1,71 @@
+# Source: https://github.com/marceloprates/prettymaps
+# General style parameters
+palette = ['#433633', '#FF5E5B']
+background_c = '#F2F4CB'
+dilate = 100
+
+# Setup plot
+fig, ax = plt.subplots(figsize = (12, 12), constrained_layout = True)
+
+layers = plot(
+    'Austin, Texas, United States', radius = 1100,
+    ax = ax,
+    
+    layers = {
+            'perimeter': {},
+            'streets': {
+                'width': {
+                    'motorway': 5,
+                    'trunk': 5,
+                    'primary': 4.5,
+                    'secondary': 4,
+                    'tertiary': 3.5,
+                    'cycleway': 3.5,
+                    'residential': 3,
+                    'service': 2,
+                    'unclassified': 2,
+                    'pedestrian': 2,
+                    'footway': 1,
+                },
+                'circle': False, 'dilate': dilate
+            },
+            'building': {'tags': {'building': True, 'landuse': 'construction'}, 'union': True, 'circle': False, 'dilate': dilate},
+            'water': {'tags': {'natural': ['water', 'bay']}, 'circle': False, 'dilate': dilate},
+            'forest': {'tags': {'landuse': 'forest'}, 'circle': False, 'dilate': dilate},
+            'green': {'tags': {'landuse': ['grass', 'orchard'], 'natural': ['island', 'wood'], 'leisure': 'park'}, 'circle': False, 'dilate': dilate},
+            'beach': {'tags': {'natural': 'beach'}, 'circle': False, 'dilate': dilate},
+            'parking': {'tags': {'amenity': 'parking', 'highway': 'pedestrian', 'man_made': 'pier'}, 'circle': False}
+        },
+        drawing_kwargs = {
+            'perimeter': {'fill': False, 'lw': 0, 'zorder': 0},
+            'background': {'fc': background_c, 'zorder': -1},
+            'green': {'fc': '#8BB174', 'ec': '#2F3737', 'hatch_c': '#A7C497', 'hatch': 'ooo...', 'lw': 1, 'zorder': 1},
+            'forest': {'fc': '#64B96A', 'ec': '#2F3737', 'lw': 1, 'zorder': 2},
+            'water': {'fc': '#a8e1e6', 'ec': '#2F3737', 'hatch_c': '#9bc3d4', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
+            'beach': {'fc': '#FCE19C', 'ec': '#2F3737', 'hatch_c': '#d4d196', 'hatch': 'ooo...', 'lw': 1, 'zorder': 3},
+            'parking': {'fc': background_c, 'ec': '#2F3737', 'lw': 1, 'zorder': 3},
+            'streets': {'fc': '#2F3737', 'ec': '#475657', 'alpha': 1, 'lw': 0, 'zorder': 4},
+            'building': {'palette': palette, 'ec': '#2F3737', 'lw': .5, 'zorder': 5},
+            
+        },
+
+        osm_credit = {'x': .405, 'y': .68, 'color': '#2F3737'}
+)
+
+# Set bounds
+xmin, ymin, xmax, ymax = layers['perimeter'].bounds
+dx, dy = xmax-xmin, ymax-ymin
+a = .2
+ax.set_xlim(xmin+a*dx, xmax-a*dx)
+ax.set_ylim(ymin+a*dy, ymax-a*dy)
+
+ax.text(
+    xmin+.39*dx, ymin+.305*dy,
+    ' '*3 + 'Stad van de Zon,\nHeerhugowaard, Netherlands',
+    color = '#2F3737',
+    zorder = 6, rotation = +1.75,
+    fontproperties = fm.FontProperties(fname = '../assets/Permanent_Marker/PermanentMarker-Regular.ttf', size = 24)
+)
+
+#plt.savefig('../prints/heerhugowaard.png')
+#plt.savefig('../prints/heerhugowaard.svg')
